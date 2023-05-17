0935d282a2e8b11085a0f14d78dc36be981af84d
PranayDange <pranaydange04@gmail.com>
2023-05-17 07:58:15 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        //  new Student();
+        Student student = Student.getStudent();
+        System.out.println(student.hashCode());
+
+        Student student1 = Student.getStudent();
+        System.out.println(student1.hashCode());
+
+        Teacher teacher = Teacher.getTeacher();
+        System.out.println(teacher.hashCode());
+
+        Teacher teacher1 = Teacher.getTeacher();
+        System.out.println(teacher1.hashCode());
+    }
+
+}
